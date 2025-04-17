e72049e974f88f1917c290b0cc8d53789ab8af9d
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2025-04-17 19:39:43 -0300

    main feita, falta as funções

--- Arvore/ABB/abb.c
+++ Arvore/ABB/abb.c
@@ -1,202 +1,159 @@
-// Arvore binaria de Busca --> arvore que é ordenada, mas nao balanceada
+/* QUESTÕES PEDIDAS :
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-// estrutura de uma árvore
-
-typedef struct arvore{
-    int info;
-    struct arvore *esquerda;
-    struct arvore *direita;
-} Arvore;
-
-int existe(Arvore*a, int x){
-    if(a == NULL)
-        return 0;   // caso basem nao existe
-    else{
-        if(x == a->info)
-            return 1;
-        if(x < a -> info)
-            return existe(a->esquerda,x );
-        else{
-            return existe(a->direita, x);
-        }
-    }
-}
-
-// imprimir é a mesma coisa que uma arvore comum, sendo pre, in e pos order
+1 - Ler uma árvore de um arquivo fornecido pelo usuário
+2 - Imprimir a árvore (Pré-ordem, Em-Ordem, Pós-ordem, em largura)
+3 - Verificar se um elemento x existe na árvore
+4 - Imprimir o nível de um nó x
+5 - Imprimir as folhas menores que um valor x
+6 - Inserir um nó x na árvore
+7 - Remover um nó x da árvore
+8 - Sair
 
 
-// imprimir valores que sao maiores ou iguais a x
-void imprimirMaioresIguais(Arvore *a, int x){
-    if(a != NULL){
-        if(a -> info >= x){  // comparando a raiz ao x
-            printf("%d", a->info);
-            imprimirMaioresIguais(a->direita, x);
-            imprimirMaioresIguais(a->esquerda, x);
-        }
-        else{
-            imprimirMaioresIguais(a->direita, x);
-        }
-    }    
-}
-
-// imprimir elementos de uma abb maiores que x e menores que y
-void Imprimir(Arvore *a, int x, int y){
-    if(a!= NULL){   
-        if(a-> info <= x)
-            Imprimir(a->direita, x , y);
-        if(a -> info >= y){
-            Imprimir(a->esquerda, x, y);
-        }
-        else{
-            printf("%d",a->info);
-            Imprimir(a->esquerda, x , y);
-            Imprimir(a->direita, x , y);
-        }
-    }
-}
-
-// toda vez que inserir um elemento, precisa manter a ordenação
-// estrategia --> percorrer a abb ate encontrar NULL. entao inserir o elemento
-
-Arvore *Inserir(Arvore *a, int x){
-    if(a == NULL) {   // se chegar a null, ta na hora de inserir o elemento
-        a = (Arvore*)malloc(sizeof(Arvore));
-        a->info == x;
-        a->esquerda = NULL;
-        a->direita = NULL;
-    }
-    else{
-        if(a -> info <= x)
-            a->esquerda = Inserir(a->esquerda, x);  // necessario colocar o igual, pois como vai alterar a lista, é necessario atualizar o caminho dela
-        else
-            a->direita = Inserir(a->direita, x);    
-    }
-    return a;   
-}
-
-// quando for remover um elemento, mantendo a ordenaçao, precisamos considerar 3 coisas:
-// 1 -> no a ser removido é folha ( so remover)
-// 2-> no a ser removido possui 1 filho nulo    --> encontra o no, junta o filho nao nulo ao pai e remove
-// 3 -> no nao possui filhos nulos  -> encontra o no, encontra o maior elemento y da subarvore da esquerda, substitui x por y, remove y da subarvore da esquerda de x
-
-/*
-Arvore* Remover(Arvore* a, int x){
-    if(a!= NULL){
-        if(a->info == x){
-            // primeiro caso:
-            if(a->esquerda == NULL && a->direita == NULL){
-                free(a);
-                return NULL;
-            }
-            // segundo caso
-            else if(a->esquerda == NULL){
-                Arvore *aux = a->esquerda;
-                free(a);
-                return aux;
-            }
-            else if(a->direita == NULL){
-                Arvore *aux = a->direita;
-                free(a);
-                return aux;
-            }
-            else{   // terceiro caso
-                Arvore *aux = a->esquerda;
-                while(aux -> direita != NULL){  // subarvore da esquerda, pra achar o maior
-                    aux = aux ->direita;
-                }
-                a->info = aux->info;        // atribuicao de valor, pra tirar depois
-                a -> esquerda = Remover(a->esquerda, aux->info);    // tirou
-            }
-            else if ( x < a->info)
-                a-> esquerda = Remover(a->esquerda, x);
-            else
-                a->direita = Remover(a->direita, x);
-            return a;
-        }
-    }
-}
 */
 
 
-int altura(Arvore*a){
-    if(a == NULL)
-        return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+// criação da arvore
+
+typedef struct Arvore{
+    int info;
+    struct Arvore* esquerda;
+    struct Arvore* direita;
+}arvore;
+
+
+// Função para o arquivo txt
+
+arvore *LerArvore(FILE *ABB) 
+{
+    char c;
+    int num;
+
+    fscanf(ABB,"%c",&c);    // ler '('
+    fscanf(ABB,"%d",&num);  // ler numero
+
+    if(num == -1){
+        fscanf(ABB,"%c",&c); // se for null, vai fechar com )
+        return NULL; 
+    }
     else{
-        int he = altura(a->esquerda);
-        int hd = altura(a->direita);
-        if(he > hd)
-            return he + 1;
-        if(he < hd)
-            return hd + 1;
+        arvore *a= (arvore*)malloc(sizeof(arvore));
+        a->info = num;
+        a->esquerda = LerArvore(ABB);
+        a->direita = LerArvore(ABB);
+        fscanf(ABB,"%c",&c);    // ler o ')'
+        return a;
     }
 }
 
-void ImprimirNivel(Arvore *a, int cont, int nivel){
+
+// funções para imprimir a arvore
+
+void preordem(arvore*a)
+{
     if(a!=NULL){
-        if(cont == nivel){
-            printf("%d",a->info);
-        }
-        else{
-            ImprimirNivel(a->esquerda, cont + 1, nivel +1);
-            ImprimirNivel(a->direita, cont +1, nivel +1);
-        }
+        printf("%d ",a->info);
+        preordem(a->esquerda);
+        preordem(a->direita);
     }
 }
 
-Arvore * Inserir2(Arvore*a, int x){
-    if(a == NULL){  // se tiver chegar em NULL, precisamos alocar memoria para ele
-        Arvore *no = (Arvore*)malloc(sizeof(Arvore));
-        no -> info = x;
-        no -> esquerda = NULL;
-        no -> direita = NULL;
-    }
-    else{
-        if(a->info <= x){
-            a->esquerda = Inserir2(a->esquerda, x);
-        }
-        else{
-            a->direita = Inserir(a->direita, x);
-        }
-    }
-    return a;
-}
+void emordem(arvore *a)
 
-void Imprimirfolhasmenores(Arvore*a, int x){
-    if(a!= NULL){
-        if(a->info < x){
-            if(a->esquerda == NULL && a->direita == NULL){
-                printf("%d", a->info);
-                Imprimirfolhasmenores(a->esquerda, x);
-                Imprimirfolhasmenores(a->direita,x);
+
+// criação da main
+
+int main()
+{
+    arvore *a = NULL;
+    FILE *ABB = fopen("ABB.txt","rt");
+
+    int escolha;
+    int escolha_ordem;
+    while(escolha!=8){
+ 
+        printf("\n\nBem vindo ao Menu. Qual opcao deseja escolher ? (1 primeiro para ler a arvore) ");
+        printf("\n\n1- Ler uma arvore");
+        printf("\n2- Imprimir a arvore");
+        printf("\n3- Verificar se um elemento x existe na arvore");
+        printf("\n4- Imprimir o nivel de um no x");
+        printf("\n5- Imprimir as folhas menores que um valor x");
+        printf("\n6- Inserir um no x na arvore");
+        printf("\n7- Remover um no x da arvore");
+        printf("\n8- Sair\n\n");
+        escolha = 0;
+        scanf("\n%d",&escolha);
+
+        switch(escolha){
+            case 1: // ler arvore
+            if(ABB == NULL)
+                printf("Erro ao abrir o arquivo");
+            else{
+                a = LerArvore(ABB);
+                fclose(ABB);
+                if(a != NULL)
+                    printf("Arvore lida com sucesso !!!");
+                else
+                    printf("Erro: arvore vazia ou mal formatada");
+                break;
             }
+                break;
+            case 2: // imprimir a arvore
+                while(escolha_ordem != 5){
+
+                    printf("Qual metodo deseja imprimir a arvore?\n\n");
+                    printf("1- Pre-ordem\n");
+                    printf("2- Em-ordem\n");
+                    printf("3- Pos-ordem\n");
+                    printf("4- Em largura\n");
+                    printf("5- Voltar ao menu\n\n");
+                    escolha_ordem = 0;
+                    scanf("%d",&escolha_ordem);
+                    switch(escolha_ordem){
+                        case 1: // pre ordem
+                            break;
+                        case 2 :    // em ordem
+                            break;
+                        case 3: // pos ordem
+                            break;
+                        case 4: // em largura
+                            break;
+                        case 5: // voltar ao menu
+                            break;
+                        default:    // numero invalido
+                            printf("Opcao invalida . Tente novamente");
+                            break;
+                    }
+                }
+                break;
+            case 3: // verificar se um elemento x existe na arvore
+                printf("\n\n Qual numero quer verificar ? ");
+                int num;
+                scanf("%d",&num);
+                break;
+            case 4: // imprimir o nivel de um no x
+                break;
+            case 5: // imprimir folhas menores que um valor x
+                break;
+            case 6: // inserir um no x na arvore
+                printf("Qual numero quer inserir ? ");
+                int inserir;
+                scanf("%d",&inserir);
+                break;
+            case 7: // remover um no x da arvore
+                printf("Qual numero quer remover ?");
+                int remover;
+                scanf("%d",&remover);
+                break;
+            case 8: // sair do programa
+                printf("Saindo do menu");
+                break;
+            default:
+                printf("Opcao invalida . Tente novamente");
+                break;
         }
-        else
-            Imprimirfolhasmenores(a->esquerda, x);
     }
 }
-
-
-int nivelno(Arvore*a, int x){   
-    if(a != NULL){
-        if(a -> info == x)
-            return 0;
-        else if( x < a -> info){
-            return nivelno(a->esquerda, x) + 1;
-        }
-        else{
-            return nivelno(a->direita, x) + 1;
-        }
-    }   
-}
-
-// para garantir que seja uma ABB, temos que garantir que cada no:
-// valores da subarvore da esquerda sejam menores que o no
-// subarvore da direita seja maior que o no
-// tem que valer para todos os nos da arvore
-
-
-
