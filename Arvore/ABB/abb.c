e58cfc255ec27532060fdf93aad9b0042bbd25b4
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2025-04-18 01:26:24 -0300

    Funções feitas
    falta implementar na main

--- Arvore/ABB/abb.c
+++ Arvore/ABB/abb.c
@@ -1,15 +1,16 @@
 /* QUESTÕES PEDIDAS :
 
-1 - Ler uma árvore de um arquivo fornecido pelo usuário
-2 - Imprimir a árvore (Pré-ordem, Em-Ordem, Pós-ordem, em largura)
-3 - Verificar se um elemento x existe na árvore
-4 - Imprimir o nível de um nó x
-5 - Imprimir as folhas menores que um valor x
-6 - Inserir um nó x na árvore
-7 - Remover um nó x da árvore
-8 - Sair
+1 - Ler uma árvore de um arquivo fornecido pelo usuário --> feito
+2 - Imprimir a árvore (Pré-ordem, Em-Ordem, Pós-ordem, em largura)--> feito
+3 - Verificar se um elemento x existe na árvore --> feito
+4 - Imprimir o nível de um nó x --> feito
+5 - Imprimir as folhas menores que um valor x  --> feito
+6 - Inserir um nó x na árvore --> feito
+7 - Remover um nó x da árvore --> feito
+8 - Sair --> feito
 
 
+OBSERVAÇÃO : FALTA IMPLEMENTAR NA MAIN A MAIORIA
 */
 
 
@@ -62,10 +63,164 @@ void preordem(arvore*a)
 }
 
 void emordem(arvore *a)
+{
+    if(a!= NULL){
+        emordem(a->esquerda);
+        printf("%d ", a->info);
+        emordem(a->direita);
+    }
+}
+
+void posordem(arvore *a)
+{
+    if(a!=NULL){
+        posordem(a->esquerda);
+        posordem(a->direita);
+        printf("%d ",a->info);
+    }
+}
+
+int altura(arvore *a)
+{
+    if(a == NULL)
+        return 0;
+    else{
+        int he = altura(a->esquerda);
+        int hd = altura(a->direita);
+        if(he > hd)
+            return he + 1;
+        else
+            return hd + 1;
+    }
+}
+
+int imprimir_nivel_no(arvore*a, int x, int cont)    // fiz uma versão melhor da mesma função da arvore binaria
+{
+    if(a == NULL)
+        return -1;
+    else{
+        if(a->info == x)
+            return cont;
+
+        int nivel_esquerda = imprimir_nivel_no(a->esquerda, x , cont + 1);
+        int nivel_direita = imprimir_nivel_no(a->direita, x , cont + 1 );
+
+        if(nivel_esquerda != -1)
+            return nivel_esquerda;
+        else
+            return nivel_direita;
+    }
+}
+
+int imprimir_nivel(arvore*a, int cont, int nivel)
+{
+    if(a!= NULL){
+        if(cont == nivel)
+            printf("%d ",a->info);
+        else{
+            imprimir_nivel(a->esquerda, cont + 1, nivel);
+            imprimir_nivel(a->direita, cont + 1, nivel);
+        }
+    }
+}
+void imprimir_largura(arvore*a)
+{
+    if(a!= NULL){
+        for(int i = 0; i < altura(a); i++){
+            imprimir_nivel(a,0,i);
+            printf("\n");
+        }
+    }
+}
+
+int existe(arvore *a, int x)    // necessario tirar proveito da ordenação
+{
+    if(a == NULL)
+        return 0;
+    else{
+        if(a->info == x)
+            return 1;
+        else if(x < a->info)
+            existe(a->esquerda,x);
+        else
+            existe(a->direita,x);
+    }
+}
+
+void folhas(arvore*a, int x)    // imprimir folhas menores que x
+{
+    if(a!= NULL){
+        if(a->esquerda == NULL && a->direita == NULL){
+            if(a->info < x)
+                printf("%d ",a->info);
+            else
+                folhas(a->esquerda, x);
+                folhas(a->direita, x);
+        }
+    }
+}
+
+arvore* inserir(arvore*a, int x)
+{
+    if(a == NULL){
+        a = (arvore*)malloc(sizeof(arvore));
+        a->info = x;
+        a->esquerda = NULL;
+        a->direita = NULL;
+    }
+    else if(x <= a->info){
+        a->esquerda = inserir(a->esquerda,x);
+    } else{
+        a->direita = inserir(a->direita,x);
+    }
+}
+
+arvore* remover(arvore* a, int x) {
+    if (a == NULL)
+        return NULL;
+
+    if (x < a->info) {
+        a->esquerda = remover(a->esquerda, x);
+    }
+    else if (x > a->info) {
+        a->direita = remover(a->direita, x);
+    }
+    else {
+        // Achou o nó a remover
+        if (a->esquerda == NULL && a->direita == NULL) {
+            // Caso 1: folha
+            free(a);
+            return NULL;
+        }
+        else if (a->esquerda == NULL) {
+            // Caso 2: só tem filho à direita
+            arvore* aux = a->direita;
+            free(a);
+            return aux;
+        }
+        else if (a->direita == NULL) {
+            // Caso 2: só tem filho à esquerda
+            arvore* aux = a->esquerda;
+            free(a);
+            return aux;
+        }
+        else {
+            // Caso 3: dois filhos
+            arvore* aux = a->esquerda;
+            while (aux->direita != NULL)
+                aux = aux->direita;
+
+            a->info = aux->info;
+            a->esquerda = remover(a->esquerda, aux->info);
+        }
+    }
+
+    return a;
+}
 
 
 // criação da main
-
+ 
 int main()
 {
     arvore *a = NULL;
