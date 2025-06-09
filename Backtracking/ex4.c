7629e52b1d039f0a5e2a91487b7e23ca555d9a95
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2025-06-09 19:31:51 +0000

    corrigindo ex4.c

--- Backtracking/ex4.c
+++ Backtracking/ex4.c
@@ -1,17 +1,20 @@
 //Igual o ex1, mas que conte o numero de vezes que acontece
 
-// dando erro na hora da impressão, perguntar para o professor
+// dando erro na hora da impressão, perguntar para o igor --> corrigido, era só passar cont_total como global e não como local
 
 #include <stdio.h>
 
+int cont_total = 0;
+
 int cont(char *vet, int pos, int n, char *vetaux) {
-    int cont_total = 0;
+
+    
     if(pos == n){
         int cont_a = 0;
         int cont_e= 0;
         int cont_i = 0;
         for(int i = 0; i < n; i++){
-            if(vet[i] = 'a')
+            if(vet[i] == 'a')
                 cont_a++;
             else if(vet[i] == 'e')
                 cont_e++;
@@ -24,7 +27,7 @@ int cont(char *vet, int pos, int n, char *vetaux) {
     }
     else{
         for(int i = 0; i <= n; i++){
-            vet[pos] == vetaux[i];
+            vet[pos] = vetaux[i];
             cont(vet, pos+1,n, vetaux);
         }
     }
@@ -32,9 +35,9 @@ int cont(char *vet, int pos, int n, char *vetaux) {
 }
 
 int main(){
-    int n = 3;
+    int n = 3;  // basta mudar essa variavel pra mudar o tamanho das combinações
     char vet[n];
     char vetaux[] ={'a','e','i'};
     int total = cont(vet,0,n,vetaux);
-    printf("Numero de vezes que isso acontece: %d",total);
+    printf("Numero de vezes que existem mais a's do que e's e i's: %d \n",total);
 }
